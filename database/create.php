3bc5dfb5234d02b1d24bb939d00330718e3b6dc7
Charounson Saintilus <cs10c@my.fsu.edu>
2014-03-18 08:29:51 -0400

    New queries for tables were added.

--- database/create.php
+++ database/create.php
@@ -4,6 +4,19 @@ CREATE DATABASE upscale;
 
 USE upscale;
 
+CREATE TABLE restaurants (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(60) NOT NULL,
+	address VARCHAR(200) UNIQUE NOT NULL,
+	phone_number VARCHAR(20) NOT NULL,
+	longitude INT NOT NULL,
+	latitude INT NOT NULL,
+	reservation_time INT NOT NULL,
+	min_notice TIME NOT NULL,
+	max_notice TIME NOT NULL,
+	status BOOL NOT NULL,
+	);
+
 CREATE TABLE users (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(60) NOT NULL,
@@ -21,6 +34,32 @@ CREATE TABLE managers (
 	password CHAR(32) NOT NULL
 	);
 	
+CREATE TABLE reservations (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	restaurant_id INT,
+	FOREIGN KEY (restaurant_id)
+		REFERENCES restaurants(id)
+		ON DELETE CASCADE
+	date DATE,
+	start_time TIME
+	number_of people INT,
+	);
+	
+CREATE TABLE tables (
+	
+	);
+
+CREATE TABLE hours (
+	
+	);
+
+CREATE TABLE special_hours (
+	
+	);
+	
+CREATE TABLE tables_in_reservation (
+	
+	);
 	
 EOD;
 ?>
