87574ca120497dd828f5b4b4d6fc1261c5b26dea
Adam Greenstein <adamgreenstein@comcast.net>
2014-04-20 16:35:52 -0400

    Updated 404 page

--- templetes/404.php
+++ templetes/404.php
@@ -2,15 +2,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Untitled Document</title>
+<title>Page not found</title>
 </head>
 
 <body>
-404 page not found
-<pre>
-<?php 
-print_r($_SESSION);
-?>
-</pre>
+	404 page not found
 </body>
 </html>
